61df604e55a247a459dcbc09dce72bf66fdf2da9
Sven Hesse <drmccoy@drmccoy.de>
2014-06-14 00:49:23 +0200

    MAIN: Add a stubby main()

---
+++ src/cokteladl2vgm.cpp
@@ -0,0 +1,27 @@
+/* CoktelADL2VGM - Tool to convert Coktel Vision's AdLib music to VGM
+ *
+ * CoktelADL2VGM is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * CoktelADL2VGM is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CoktelADL2VGM. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file cokteladl2vgm.cpp
+ *  The project's main entry point.
+ */
+
+int main(int argc, char **argv) {
+	return 0;
+}
